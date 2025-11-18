4ad5df8bd3dbe78d62d77b14d8743869467b2c97
jimmypan <jimmyp@ms13.hinet.net>
2025-11-18 08:48:43 +0800

    漏掉A01PassWord

---
+++ RED/BKND/A01PassWord.php
@@ -0,0 +1,32 @@
+﻿<?php
+   header("Content-Type:text/html; charset=utf-8");   
+
+ include("../../include/BKND/mysqli_server.php");               //引用檔   
+	
+	 $sql3="SELECT `F02`,`F00`  FROM `a01` WHERE binary `F01` ='".$_POST['filename']."' ";	 	 
+
+    $arr=array();	
+    $sql4=@mysqli_query($link,$sql3); 
+	$rows=@mysqli_num_rows($sql4);
+	if($rows>0){
+	    while ($list3=mysqli_fetch_assoc($sql4)){
+		 
+		    $atr = array('passWord'=>$list3['F02'],'userId'=>$list3['F00']);                              
+		    array_push($arr,$atr);
+	    }
+	}else{
+	    $atr = array('passWord'=>"",'userId'=>"");                              
+		    array_push($arr,$atr);
+	}
+	mysqli_close($link);
+	     $arr = array_values($arr);
+         $json_string1 = json_encode($arr); 
+         echo $json_string1;	 
+        // echo "srchStockNo($json_string1)";    
+       
+		 
+ 		 
+          
+?>  
+
+ 
